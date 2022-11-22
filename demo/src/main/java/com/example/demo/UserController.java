53853b3bfd38308d7ed6f2a29aae2995ff58df3e
FCG\fdattolo <fdattolo@florence-consulting.it>
2022-11-22 16:08:50 +0100

    Upload modifies

--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,4 +18,41 @@ public class UserController {
     @GetMapping("/users")
     Iterable<User> getAllUsers(){return userRepository.findAll();}
 
+    @PostMapping("/users")
+    User createNewUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @PutMapping("/users/{id}")
+    User updateUser(@RequestParam Long id, @RequestBody User userDto){
+
+        User userToUpdate = userRepository.findById(id).orElseThrow();   // va bene gestire così l'eccezione??
+        userToUpdate.setNome(userDto.getNome());
+        userToUpdate.setCognome(userDto.getCognome());
+        userToUpdate.setMail(userDto.getMail());
+        userToUpdate.setIndirizzo(userDto.getIndirizzo());
+
+        return userRepository.save(userToUpdate);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    void deleteUser(@RequestParam Long userId){
+        //userRepository.deleteById(userId);
+        //oppure
+        User userToDelete = userRepository.findById(userId).orElseThrow();
+        //userRepository.deleteById();
+        userRepository.delete(userToDelete);
+    }
+
+    @GetMapping("/users/{nome}{cognome}") //QueryParam
+    List<User> getUsersSearched(@RequestParam String nome, @RequestParam String cognome){
+        List<User> selectedUsers = userRepository.findByNomeAndCognome(nome, cognome);
+
+        if(!selectedUsers.isEmpty())
+            return selectedUsers;
+        else
+            throw new ResponseStatusException( //ControllerAdvace
+                    HttpStatus.NOT_FOUND, "entity not found"
+            );
+    }
 }
--- demo/src/main/java/com/example/demo/UserRepository.java
+++ demo/src/main/java/com/example/demo/UserRepository.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    public List<User> findByNomeAndCognome(String nome, String cognome);
 }
